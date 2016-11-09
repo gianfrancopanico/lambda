d5bfc284d2655f9d8b380ee27a3fca9ef1ac0c01
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2016-11-09 14:27:54 +0000

    Create ec2_destroy_instances.py

---
+++ ec2_destroy_instances.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    print ("Received event: " + json.dumps(event, indent=2))
+    print ("************************************************")
+    
+    ec2 = boto3.resource('ec2')
+    
+    
+    print "Event Region :", event['region']
+    
+    event_time = event['time']
+    print "Event Time :", event_time
+    
+    time = event_time.split('T')
+    t = time[1]
+    t = t.split(':')
+    hour = t[0]
+    
+    filters = [
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    instances = ec2.instances.filter(Filters=filters)
+    
+    RunningInstances = [instance.id for instance in instances]
+    
+    if int(hour) > 19:
+        print ("Right hour: " + int(hour))
+        
+    
+    
+    if len(RunningInstances) > 0 and int(hour) > 19:
+            shuttingDown = ec2.instances.filter(InstanceIds=RunningInstances).stop()
+            print shuttingDown
