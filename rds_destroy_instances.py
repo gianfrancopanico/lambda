5d48014edfd5e5f5616b044899d0b4bfc20fb57a
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2016-11-09 14:56:02 +0000

    Update rds_destroy_instances.py
    Some little cleaning, removed useless output

--- rds_destroy_instances.py
+++ rds_destroy_instances.py
@@ -4,24 +4,25 @@ import boto3
 
 def lambda_handler(event, context):
     # print ("Received event: " + json.dumps(event, indent=2))
-    print ("************************************************")
+    # print ("************************************************")
     
     rdscon = boto3.client('rds')
     rdb = rdscon.describe_db_instances().get(
         'DBInstances',[] 
         )
         
-    rdblist = len(rdb)
+    # rdblist = len(rdb)
     # print "Found ", rdblist, " running servers"    
         
     for dbinstance in rdb:
             inst_id=dbinstance['DBInstanceIdentifier']
-            snap_id=(inst_id + "-20" + datetime.datetime.now().strftime("%y%m%d-%H%M") ) 
+            snap_id=( inst_id + "-20" + datetime.datetime.now().strftime("%y%m%d-%H%M") ) 
             print "Processing instance: ", inst_id
             print ">>> creating snapshot ", snap_id
+            
             rdscon.delete_db_instance(
                 DBInstanceIdentifier=inst_id, 
                 FinalDBSnapshotIdentifier=snap_id,
                 SkipFinalSnapshot=False
                 )
-    
+   
