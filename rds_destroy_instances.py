5dca9ca839149302e4008b8ddcefcff198f636e6
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2016-11-11 12:23:28 +0000

    Update rds_destroy_instances.py
    added tagging for write-protection

--- rds_destroy_instances.py
+++ rds_destroy_instances.py
@@ -5,25 +5,36 @@ import boto3
 def lambda_handler(event, context):
     # Check out the code below whenever you need to analyse the json output from the event.
     # print ("Received event: " + json.dumps(event, indent=2))
-    # print ("************************************************")
     
     rdscon = boto3.client('rds')
+    
+    # list any rds instances running 
     rdb = rdscon.describe_db_instances().get(
         'DBInstances',[] 
         )
         
-    # rdblist = len(rdb)
-    # print "Found ", rdblist, " running servers"    
-        
     for dbinstance in rdb:
             inst_id=dbinstance['DBInstanceIdentifier']
-            snap_id=( inst_id + "-" + datetime.datetime.now().strftime("%Y%m%d-%H%M") ) 
+            snap_id=(inst_id + "-20" + datetime.datetime.now().strftime("%y%m%d-%H%M") ) 
             print "Processing instance: ", inst_id
             print ">>> creating snapshot ", snap_id
-            
             rdscon.delete_db_instance(
                 DBInstanceIdentifier=inst_id, 
                 FinalDBSnapshotIdentifier=snap_id,
                 SkipFinalSnapshot=False
                 )
-   
+            #create tags to make the actual snapshot undeleteable by other scripts
+            rdscon.add_tags_to_resource(
+                ResourceName='arn:aws:rds:eu-west-1:108652351904:snapshot:'+snap_id,
+                Tags=[
+                        {
+                            'Key': 'Write-Protected', 
+                            'Value': 'True'
+                        },
+                        {
+                            'Key': 'Contact',
+                            'Value': 'infradev-leads@sainsburys.co.uk'
+                        }
+                     ]
+                )
+            print ">>> terminating "
