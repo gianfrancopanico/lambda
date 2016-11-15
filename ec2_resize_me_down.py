a39e027893878135b2429d40432ec92cc9458c74
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2016-11-15 14:11:41 +0000

    Update ec2_resize_me_down.py
    this might even work.
    gona test it now

--- ec2_resize_me_down.py
+++ ec2_resize_me_down.py
@@ -14,26 +14,16 @@ def lambda_handler(event, context):
     
     running_instances = ec2.instances.filter(Filters=filters)
     
-    # stops all instances not tagged 'write-protected'
+    # looks for instances tagged as 'scale-down'
     for instance in running_instances: 
-        kill_me=1
         for tag in instance.tags:
-            if tag['Key'] == 'write-protected':
-                print "*** "+instance.id+" is write protected..."
-                kill_me=0
-                break        
-        if kill_me == 1:
-            print instance.id +" will be killed";
-            ids=[ instance.id ]
-            # postmortem=running_instances.filter(InstanceIds=ids).stop();
-
-# Stop the instance
-client.stop_instances(InstanceIds=[my_instance])
-waiter=client.get_waiter('instance_stopped')
-waiter.wait(InstanceIds=[my_instance])
-
-# Change the instance type
-client.modify_instance_attribute(InstanceId=my_instance, Attribute='instanceType', Value='m3.xlarge')           
- # Start the instance
-client.start_instances(InstanceIds=[my_instance])
-
+            if tag['Key'] == 'scale-down':
+                print "*** "+instance.id+" is going to be scaled down..."  
+                # stop the instance before resizing
+                ec2.stop_instances(InstanceIds=[instance.id])
+                waiter=ec2.get_waiter('instance_stopped')
+                waiter.wait(InstanceIds=[instance.id])
+                # Change the instance type
+                ec2.modify_instance_attribute(InstanceId=instance.id, Attribute='instanceType', Value='m3.xlarge')           
+                # Start the instance
+                ec2.start_instances(InstanceIds=[instance.id])
