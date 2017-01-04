96a595e7a1226f970918b47ce8c50efe32f46ba4
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2017-01-04 15:46:57 +0000

    Update ec2_create_snapshot.py

--- ec2_create_snapshot.py
+++ ec2_create_snapshot.py
@@ -4,18 +4,12 @@ import datetime
 import time
 
 account_id='852857041816'
-region_id='us-east-1'
 volume_id='vol-1ea15bce'
 
 def lambda_handler(event, context):
-
   client=boto3.client('ec2')
-  # arn_id='arn:aws:ec2:'+region_id+':'+account_id+':volume/'+volume_id
-  
   response = client.create_snapshot(VolumeId=volume_id, Description='lambda-generated-snapshot')
-  
   my_snap=response['SnapshotId']
-  
   response=client.describe_snapshots(SnapshotIds=[ my_snap ])
   
   while response['Snapshots'][0]['State'] != 'completed':
@@ -25,3 +19,4 @@ def lambda_handler(event, context):
       
   client.create_tags(Resources=[ my_snap ], Tags=[ { 'Key': 'Purgeable', 'Value': 'True' } ] ) 
   print ("Snapshot "+my_snap+" completed and tagged successfully")
+  return (0)
