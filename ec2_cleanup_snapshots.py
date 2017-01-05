aee1c8e77bf86890b619b3378f7647096f0e52f4
Gianfranco Panico <gianfranco.panico@sainsburys.co.uk>
2017-01-05 10:47:25 +0000

    Update ec2_cleanup_snapshots.py
    now it does list the snapshots. 
    todo: identify the tags and delete the Purgeable ones.

--- ec2_cleanup_snapshots.py
+++ ec2_cleanup_snapshots.py
@@ -9,27 +9,14 @@ account_id='852857041816'
 def lambda_handler(event, context):
     
     ec2 = boto3.client('ec2')
-    snap_list = ec2.describe_snapshots().get('SnapshotId',[])
-     
+    snap_list = ec2.describe_snapshots()
+    
     # compute the cut-off time for snapshots
     cut_date=datetime.datetime.now()-datetime.timedelta(days=15)
         
-    for dbsnaps in snap_list:
-            inst_id=dbsnaps['SnapshotIdentifier']
-            snap_date=datetime.datetime.strptime(str(dbsnaps['SnapshotCreateTime'])[:19], "%Y-%m-%d %H:%M:%S")
-            
-            if snap_date<cut_date:
-                # check tags first 
-                response = rdscon.list_tags_for_resource(
-                    ResourceName='arn:aws:rds:eu-west-1:'+account_id+':snapshot:'+inst_id,
-                    Filters=[]
-                    )
-                
-                # requires the tags 'Purgeable'    
-                for tag in response['TagList']:
-                   if tag['Key'] == 'Purgeable' and tag['Value']=='True':
-                     print "*** deleting arn:aws:rds:eu-west-1:"+account_id+":snapshot:" + inst_id    
-                   else: 
-                    print ">>> skipping arn:aws:rds:eu-west-1:"+account_id+":snapshot:"+inst_id 
-                    
-                    # rdscon.delete_db_snapshot(DBSnapshotIdentifier=inst_id)
+    for volsnaps in snap_list['Snapshots']:
+        snap_date=datetime.datetime.strptime(str(volsnaps['StartTime'])[:19], "%Y-%m-%d %H:%M:%S")
+        inst_id=volsnaps['SnapshotId']
+        print inst_id+" "+str(snap_date)
+           
+    return (0)
